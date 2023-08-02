2c8e00427c2cabc14451676b37c299dc3e8f20cb
Salva007 <103088037+slv14@users.noreply.github.com>
2023-08-02 12:50:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,3 @@ from gui_setup import *
 root = tk.Tk()
 app = PasswordGeneratorApp(root)
 root.mainloop()
-
-
-# cd "C:\Users\chava\Desktop\Sistemas Computacionales\Python\Proyectos\Passwords Generator"
